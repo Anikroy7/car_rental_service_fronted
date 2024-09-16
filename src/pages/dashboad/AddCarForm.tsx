b5a932679ccc8c7e7ac5ff75a51498480e38f39e
Anikroy7 <roy645800@gmail.com>
2024-09-16 16:52:11 +0600

    add car form added

--- src/pages/dashboad/AddCarForm.tsx
+++ src/pages/dashboad/AddCarForm.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
 import { FaPlus } from "react-icons/fa";
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
@@ -16,6 +17,56 @@ export default function AddCarForm() {
     const [value, setValue] = useState('');
     const [feature, setFeature] = useState('')
     const [allFeatures, setAllFeatures] = useState([]);
+    const imageTypeRegex = /image\/(png|jpg|jpeg)/gm;
+
+
+    const [imageFiles, setImageFiles] = useState([]);
+    const [images, setImages] = useState([]);
+
+    const changeHandler = (e) => {
+        e.preventDefault()
+        const { files } = e.target;
+        const validImageFiles = [];
+        for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+            if (file.type.match(imageTypeRegex)) {
+                validImageFiles.push(file);
+            }
+        }
+        if (validImageFiles.length) {
+            setImageFiles(validImageFiles);
+            return;
+        }
+        alert("Selected images are not of valid type!");
+    };
+    useEffect(() => {
+        const images = [], fileReaders = [];
+        let isCancel = false;
+        if (imageFiles.length) {
+            imageFiles.forEach((file) => {
+                const fileReader = new FileReader();
+                fileReaders.push(fileReader);
+                fileReader.onload = (e) => {
+                    const { result } = e.target;
+                    if (result) {
+                        images.push(result)
+                    }
+                    if (images.length === imageFiles.length && !isCancel) {
+                        setImages(images);
+                    }
+                }
+                fileReader.readAsDataURL(file);
+            })
+        };
+        return () => {
+            isCancel = true;
+            fileReaders.forEach(fileReader => {
+                if (fileReader.readyState === 1) {
+                    fileReader.abort()
+                }
+            })
+        }
+    }, [imageFiles]);
 
     const addToFeatures = (e) => {
         e.preventDefault()
@@ -34,6 +85,12 @@ export default function AddCarForm() {
     const removeFromFeatures = (item) => {
         setAllFeatures([...allFeatures.filter(f => f !== item)])
     }
+    const removeImage = (image, index) => {
+        const newImageFiles = imageFiles.filter((f, i) => i !== index)
+        setImageFiles([...newImageFiles])
+        // console.log(newImageFiles)
+        setImages([...images.filter(i => i !== image)])
+    }
     return (
         <div className="bg-gray-100 mx-auto  py-20 px-12 lg:px-24 shadow-xl mb-24">
             <form className="w-full">
@@ -44,7 +101,7 @@ export default function AddCarForm() {
                             <label className="uppercase tracking-wide text-black text-xs font-bold mb-2" htmlFor="application-link">
                                 Car title
                             </label>
-                            <input className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3" id="application-link" type="text" placeholder="" />
+                            <input className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3" id="application-link" type="text" placeholder="Type color title" />
                         </div>
                     </div>
                     <div className="my-3">
@@ -78,7 +135,7 @@ export default function AddCarForm() {
                                 Price Per hour*
                             </label>
                             <div>
-                                <input className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3" id="application-link" type="number" placeholder="" />
+                                <input className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3" id="application-link" type="number" placeholder="Type price/h" />
                             </div>
                         </div>
                         <div className="md:w-1/2 px-3">
@@ -86,17 +143,8 @@ export default function AddCarForm() {
                                 Color*
                             </label>
                             <div>
-                                <input className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3" id="application-link" type="text" placeholder="" />
+                                <input className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3" id="application-link" type="text" placeholder="valid color name or color values" />
                             </div>
-                        </div>
-                    </div>
-                    <div className="-mx-3 md:flex">
-                        <div className="md:w-full px-3">
-                            <label className="uppercase tracking-wide text-black text-xs font-bold mb-2" htmlFor="application-link">
-                                Images* ( you can upload mutiple images )
-                            </label>
-                            <input type="file" className="file-input file-input-bordered w-full " />
-
                         </div>
                     </div>
                     <div>
@@ -109,8 +157,7 @@ export default function AddCarForm() {
                                 onChange={(e) => setFeature(e.target.value)}
                                 type="text"
                                 placeholder="Type here"
-                                className="input input-bordered input-sm flex-grow"
-                                required
+                                className="input input-bordered input-sm flex-grow bg-gray-200 text-black border border-gray-200"
                                 value={feature}
                             />
 
@@ -121,15 +168,15 @@ export default function AddCarForm() {
                             </button>
                         </div>
                     </div>
-                    <div className="my-3">
+                    <div className="mb-3">
                         {
-                            allFeatures?.map(f => <div className="badge badge-info gap-2 mr-3 p-3">
+                            allFeatures?.map(f => <div className="badge  gap-2 mr-3 p-3">
                                 <svg
                                     onClick={() => removeFromFeatures(f)}
                                     xmlns="http://www.w3.org/2000/svg"
                                     fill="none"
                                     viewBox="0 0 24 24"
-                                    className="inline-block h-4 w-4 stroke-current">
+                                    className="inline-block h-4 w-4 stroke-current cursor-pointer">
                                     <path
                                         strokeLinecap="round"
                                         strokeLinejoin="round"
@@ -140,6 +187,47 @@ export default function AddCarForm() {
                             </div>)
                         }
                     </div>
+                    <p>
+                        <label className="uppercase tracking-wide text-black text-xs font-bold mb-2" htmlFor="application-link">
+                            Images* ( you can upload mutiple images )
+                        </label>
+                        <input
+                            className="file-input file-input-bordered w-full"
+                            type="file"
+                            id="file"
+                            onChange={(e) => changeHandler(e)}
+                            accept="image/png, image/jpg, image/jpeg"
+                            multiple
+                            style={{ color: 'transparent' }}
+                        />
+                    </p>
+
+                    {/* Display the count of files dynamically */}
+                    {imageFiles.length > 0 && <p className="my-2">{imageFiles.length} file{ imageFiles.length>1&&'(s)'} uploaded</p>}
+
+                    {
+                        images.length > 0 ?
+                            <div className="flex gap-3 flex-wrap">                                {
+                                images.map((image, index) => {
+                                    return <div className="relative w-24 group">
+                                        <div className="avatar">
+                                            <div className="w-24 rounded">
+                                                <img
+                                                    src={image}
+                                                    alt="avatar"
+                                                />
+                                            </div>
+                                        </div>
+
+                                        {/* Cross button - hidden initially, visible on hover */}
+                                        <button type="button" onClick={() => removeImage(image, index)} className="absolute top-1 right-1 text-white bg-gray-800 rounded-full p-1 opacity-0 group-hover:opacity-100 transition-opacity">
+                                            <AiOutlineClose className="w-5 h-5" />
+                                        </button>
+                                    </div>
+                                })
+                            }
+                            </div> : null
+                    }
                     <div className="-mx-3 md:flex mt-2">
                         <div className="md:w-full px-3">
                             <button className="md:w-full bg-gray-900 text-white font-bold py-2 px-4 border-b-4 hover:border-b-2 border-gray-500 hover:border-gray-100 rounded-full">
@@ -148,7 +236,7 @@ export default function AddCarForm() {
                         </div>
                     </div>
                 </div>
-            </form>
-        </div>
+            </form >
+        </div >
     );
 }
--- src/pages/dashboad/Dashboard.tsx
+++ src/pages/dashboad/Dashboard.tsx
@@ -51,7 +51,7 @@ export default function Dashboard() {
                                         </summary>
                                         <ul className="ml-7">
                                             <li>
-                                                <Link to="/admin/dashboard/manage/users/add"
+                                                <Link to="/admin/dashboard/manage/cars/add"
                                                     className={`btn btn-sm w-full justify-start mt-2 hover:bg-gray-900 hover:text-white ${paths.includes('userds') && 'bg-black text-white'}`}>
                                                     Add Car
                                                 </Link>
@@ -78,8 +78,27 @@ export default function Dashboard() {
                             <div className="flex justify-between items-center">
                                 <h1 className="text-2xl font-semibold">Dashboard</h1>
                                 <div className="flex items-center space-x-4">
-                                    <input type="text" placeholder="Search..." className="input input-bordered w-full max-w-xs" />
-                                    <button className="btn btn-primary">Search</button>
+                                    <div className="dropdown dropdown-end">
+                                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                            <div className="w-10 rounded-full">
+                                                <img
+                                                    alt="Tailwind CSS Navbar component"
+                                                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                                            </div>
+                                        </div>
+                                        <ul
+                                            tabIndex={0}
+                                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                                            <li>
+                                                <a className="justify-between">
+                                                    Profile
+                                                    <span className="badge">New</span>
+                                                </a>
+                                            </li>
+                                            <li><a>Settings</a></li>
+                                            <li><a>Logout</a></li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                         </nav>
